1256ff1eef1c0df6e7a9db1e115f2cd1834eeb27
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-01-04 10:54:23 +0900

    broadcast created, updated, and deleted quotes asynchronously

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,8 +3,17 @@ class Quote < ApplicationRecord
 
   scope :ordered, -> { order(id: :desc) }
   
-  after_create_commit -> { broadcast_prepend_to "quotes",
-                           partial: "quotes/quote",
-                           locals: { quote: self },
-                           target: "quotes" }
+  # after_create_commit -> { broadcast_prepend_to "quotes",
+  #                          partial: "quotes/quote",
+  #                          locals: { quote: self },
+  #                          target: "quotes" }
+
+  # Default target == model_name.plural
+  # Default partial == model_instance.to_partial_path // i.e. "quotes/quote"
+  # Default locals == { model_name.element.to_sym => self }
+  after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  after_update_commit -> { broadcast_replace_later_to "quotes" }
+  after_destroy_commit -> { broadcast_remove_to "quotes" } # No asynchronous equivalent
+  # Combine the three callbacks above into a single method:
+  # broadcasts_to ->(quote) { "quotes" }, inserts_by: prepend
 end
