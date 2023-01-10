f6e6ad074e88f41491e5190bfb41e0a200ea03c8
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-01-10 18:02:52 +0900

    configure turbo streams to broadcast only to authorized users

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -13,9 +13,9 @@ class Quote < ApplicationRecord
   # Default target == model_name.plural
   # Default partial == model_instance.to_partial_path // i.e. "quotes/quote"
   # Default locals == { model_name.element.to_sym => self }
-  after_create_commit -> { broadcast_prepend_later_to "quotes" }
-  after_update_commit -> { broadcast_replace_later_to "quotes" }
-  after_destroy_commit -> { broadcast_remove_to "quotes" } # No asynchronous equivalent
+  after_create_commit ->(quote) { broadcast_prepend_later_to quote.company, "quotes" }
+  after_update_commit ->(quote) { broadcast_replace_later_to quote.company, "quotes" }
+  after_destroy_commit ->(quote) { broadcast_remove_to quote.company, "quotes" } # No asynchronous equivalent
   # Combine the three callbacks above into a single method:
-  # broadcasts_to ->(quote) { "quotes" }, inserts_by: prepend
+  # broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: prepend
 end
